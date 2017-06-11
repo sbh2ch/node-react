9f6d218558c4a117e5e0a320157dec10c5e73c9a
kiost-PC\kiost <sbh2ch@gmail.com>
2017-06-11 17:42:01 +0900

    add Function

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,7 @@ import React from 'react';
 import ContactInfo from './ContactInfo';
 import ContactDeatils from './ContactDetails';
 import update from 'react-addons-update';
+import ContactCreate from "./ContactCreate";
 
 export default class Contact extends React.Component {
     constructor(props) {
@@ -101,6 +102,9 @@ export default class Contact extends React.Component {
                 <div>{mapToComponents(this.state.contactData)}</div>
                 <ContactDeatils isSelected={this.state.selectedKey != -1}
                                 contact={this.state.contactData[this.state.selectedKey]}
+                />
+                <ContactCreate
+                    onCreate={this.handleCreate}
                 />
             </div>
         );
