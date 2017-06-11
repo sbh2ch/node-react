5ac7f0287d23f6b76325cd18b29dfbab26a12823
kiost-PC\kiost <sbh2ch@gmail.com>
2017-06-11 19:30:41 +0900

    add keyPress Event

--- src/components/ContactCreate.js
+++ src/components/ContactCreate.js
@@ -13,6 +13,8 @@ export default class ContactCreate extends React.Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+
     };
 
     handleChange(e) {
@@ -35,6 +37,12 @@ export default class ContactCreate extends React.Component {
         })
     };
 
+    handleKeyPress(e) {
+        if (e.charCode === 13) {
+            this.handleClick();
+        }
+    }
+
     render() {
         return (
             <div>
@@ -43,7 +51,9 @@ export default class ContactCreate extends React.Component {
                     <input type="text" name="name" placeholder="name" value={this.state.name}
                            onChange={this.handleChange}/>
                     <input type="text" name="phone" placeholder="phone" value={this.state.phone}
-                           onChange={this.handleChange}/>
+                           onChange={this.handleChange}
+                            onKeyPress={this.handleKeyPress}
+                    />
                 </p>
                 <button onClick={this.handleClick}>create</button>
             </div>
--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -16,6 +16,7 @@ export default class ContactDetails extends React.Component {
         this.handleToggle = this.handleToggle.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleEdit = this.handleEdit.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     };
 
     handleToggle() {
@@ -42,6 +43,12 @@ export default class ContactDetails extends React.Component {
         this.setState(nextState);
     };
 
+    handleKeyPress(e) {
+        if (e.charCode === 13) {
+            this.handleToggle();
+        }
+    };
+
 
     render() {
         const details = (<div>
@@ -53,11 +60,11 @@ export default class ContactDetails extends React.Component {
             <div>
                 <p>
                     <input type="text" name="name" placeholder="name" value={this.state.name}
-                           onChange={this.handleChange}/>
+                           onChange={this.handleChange} onKeyPress={this.handleKeyPress}/>
                 </p>
                 <p>
                     <input type="text" name="phone" placeholder="phone" value={this.state.phone}
-                           onChange={this.handleChange}/>
+                           onChange={this.handleChange} onKeyPress={this.handleKeyPress}/>
                 </p>
             </div>
         );
